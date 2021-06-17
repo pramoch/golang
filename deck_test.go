ccc4d02d8930cf1cc541caa7fe2f6965ac2110eb
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2021-06-17 11:35:00 +0700

    Add test file

--- deck_test.go
+++ deck_test.go
@@ -6,6 +6,10 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 3 {
-		t.Errorf("Expected deck length of 20, but got %v", len(d))
+		t.Errorf("Expected deck length of 3, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of 'Ace of Spades', but got '%v'", d[0])
 	}
 }
