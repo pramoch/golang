5a215291d35b53a7a4792a03b5973a8a49529a73
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2021-06-19 19:47:53 +0700

    structs

--- structs/main.go
+++ structs/main.go
@@ -2,24 +2,59 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// #1
-	alex := person{"Alex", "Anderson"}
+	// alex := person{"Alex", "Anderson"}
 
 	// #2
-	michael := person{firstName: "Michael", lastName: "Anderson"}
+	// michael := person{firstName: "Michael", lastName: "Anderson"}
 
 	// #3
-	var david person
-	david.firstName = "David"
-	david.lastName = "Anderson"
+	// var david person
+	// david.firstName = "David"
+	// david.lastName = "Anderson"
 
-	fmt.Println(alex)
-	fmt.Println(michael)
-	fmt.Printf("%+v", david)
+	// fmt.Println(alex)
+	// fmt.Println(michael)
+	// fmt.Printf("%+v", david)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateNameByValue("Jimmy")
+	jim.print()
+
+	jimPointer := &jim
+	jimPointer.updateNameByReference("Jimmy")
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+	fmt.Println()
+}
+
+func (p person) updateNameByValue(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (pointerToPersion *person) updateNameByReference(newFirstName string) {
+	(*pointerToPersion).firstName = newFirstName
 }
