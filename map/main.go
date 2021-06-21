acd4d22eae008c36d502042de45ce62dd2d7af18
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2021-06-21 15:02:21 +0700

    map

--- map/main.go
+++ map/main.go
@@ -13,4 +13,18 @@ func main() {
 	colors2 := make(map[string]string)
 	colors2["white"] = "#ffffff"
 	fmt.Println(colors2)
+
+	colors3 := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
+
+	printMap(colors3)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex color for " + color + " is " + hex)
+	}
 }
